14cc0a3ed458b8f36db801fb5aed4c70df70db21
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-11 17:51:11 +0100

    fixed first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Profile from "./Components/Profile/Profile";
 import FriendList from "./Components/FriendList/FriendList";
+// import TransactionHistory from "./Components/TransactionHistory/TransactionHistory";
+
 import userData from "./assets/user.json";
-import friendData from "./assets/friends.json";
+import friendsData from "./assets/friends.json";
+// import transactionsData from "./assets/transaction.json";
 
 export default function App() {
   return (
     <>
-      {userData.map((user) => (
-        <Profile key={user.id} {...user} />
-      ))}
-      <FriendList friends={friendData} />
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friendsData} />
+      {/* <TransactionHistory items={transactionsData} /> */}
     </>
   );
 }
