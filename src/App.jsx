96cf475fa015394c5a7b6b1dc20a33b6c887734d
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-11 20:41:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Profile from "./Components/Profile/Profile";
-import FriendList from "./Components/FriendList/FriendList";
-import TransactionHistory from "./Components/TransactionHistory/TransactionHistory";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import userData from "./assets/user.json";
 import friendsData from "./assets/friends.json";
